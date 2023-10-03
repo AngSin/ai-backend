224fb39cb366b433d05588d17ee940c596622e76
Angad Singh <angad.singh@imedia24.de>
2023-10-03 22:37:05 +0100

    Initial commit

---
+++ test/ai-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AiBackend from '../lib/ai-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ai-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AiBackend.AiBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
